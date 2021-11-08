a9a49d76a374702e87e70c4be3594a07a3b73d43
wangsiyao1693 <wangsiyao1693@ichinae.com>
2021-11-08 14:52:44 +0800

    add insert sort

--- sort/sort.c
+++ sort/sort.c
@@ -31,14 +31,41 @@ int quick_sort(int *array, int low, int high)
             high--;
         }
         array[low] = array[high];
+        // for (int i = 0; i < 10; i++) {
+        //     printf("%d\t", array[i]);
+        // }
+        // printf("\n");
         while (low < high && array[low] <= pivot) {
             low++;
         }
         array[high] = array[low];
+        // for (int i = 0; i < 10; i++) {
+        //     printf("%d\t", array[i]);
+        // }
+        // printf("\n");
     }
     array[low] = pivot;
+    // for (int i = 0; i < 10; i++) {
+    //     printf("%d\t", array[i]);
+    // }
+    // printf("\n");
     quick_sort(array, start, low-1);
     quick_sort(array, low + 1, end);
+    return 0;
+}
+
+int insert_sort(int *array, int size)
+{
+    int j;
+    for (j = 1; j < size; j++) {
+        int value = array[j];
+        int posation = j;
+        while (posation > 0 && value < array[posation - 1]) {
+            array[posation] = array[posation - 1];
+            posation--;
+        }
+        array[posation] = value;
+    }
     return 0;
 }
 
@@ -47,8 +74,13 @@ int main()
     int array[10] = {5, 9, 5, 8, 7, 2, 4, 0, 3, 1};
 
     quick_sort(array, 0, 9);
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         printf("%d\t", array[i]);
     }
+    printf("\n");
+    insert_sort(array, 10);
+    for (int i = 0; i < 10; i++) {
+        printf("%d\t", array[i]);
+    }
+    printf("\n");
 }
